fe760800684cad1fef837253e9f31180febb620f
DADJOD <kamu57@mail.ru>
2023-03-18 17:35:51 +0300

    commit 1

---
+++ app/src/main/java/com/example/threads/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.threads
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var button2: Button
+    private val handler: Handler = Handler(Looper.getMainLooper())
+    private val textDone = "done"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun doSomethingLong() {
+        button2 = findViewById(R.id.button2)
+        try {
+            println("long operation start")
+            Thread.sleep(2000)
+            handler.post {
+                run {
+                    button2.text = textDone
+                }
+            }
+
+            println("long operation stop")
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun buttonClick(view: View) {
+//        val t1 = WorkerThread()
+//        t1.start()
+        doSomethingLong()
+    }
+
+//    class WorkerThread : Thread() {
+//        private var main: MainActivity = MainActivity()
+//        override fun run() {
+//            main.doSomethingLong()
+//        }
+//    }
+}
